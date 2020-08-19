6a72541d70cab91b9d6cb325c359067d7b00d660
Yoooi <kamos21@protonmail.com>
2020-08-19 23:43:02 +0200

    Update
    * Move settings to dictionary
    * Refactor device loop to AbstractDevice and TipMenuDevice
    * Add ExcitementDevice
    * Add action offset and delay

--- chaturbate-osr.py
+++ chaturbate-osr.py
@@ -11,17 +11,19 @@ import sys
 import time
 import websockets
 
-devicePort = 'COM4'
-deviceInterval = 1/60
-deviceAxisRange = {
-    'L0': [0.3, 0.7],
-    'L1': [0.0, 1.0],
-    'L2': [0.0, 1.0],
-    'R0': [0.0, 1.0],
-    'R1': [0.4, 0.6],
-    'R2': [0.4, 0.6],
-    'V0': [0.0, 1.0],
-    'V1': [0.0, 1.0]
+deviceSettings = {
+    'port': 'COM4',
+    'interval': 1 / 60,
+    'range': {
+        'L0': [0.0, 1.0],
+        'L1': [0.0, 1.0],
+        'L2': [0.0, 1.0],
+        'R0': [0.0, 1.0],
+        'R1': [0.0, 1.0],
+        'R2': [0.0, 1.0],
+        'V0': [0.0, 1.0],
+        'V1': [0.0, 1.0]
+    }
 }
 
 if len(sys.argv) != 2:
@@ -34,6 +36,7 @@ logger.setLevel(logging.INFO)
 logger.info('loading settings: %s', sys.argv[1])
 with open(sys.argv[1]) as f:
     settings = json.load(f)
+    settings['device'] = deviceSettings
 
 def clamp(v, a, b):
     return max(min(v, b), a)
@@ -44,36 +47,123 @@ def clamp01(v):
 def lerp(a, b, t):
     return a * (1 - t) + b * t
 
-async def device(loop, queue):
-    def getCommand(axis, value):
-        range = deviceAxisRange[axis]
+class AbstractDevice():
+    def __init__(self, loop, queue):
+        self.loop = loop
+        self.queue = queue
+
+        try:
+            self.device = serial.Serial(settings['device']['port'], 115200)
+        except Exception as e:
+            self.device = None
+            logger.fatal(e)
+
+        self.defaultPositions = {
+            'L0': 0.5,
+            'L1': 0.5,
+            'L2': 0.5,
+            'R0': 0.5,
+            'R1': 0.5,
+            'R2': 0.5,
+            'V0': 0.0,
+            'V1': 0.0
+        }
+        self.positions = self.defaultPositions.copy()
+        self.updateDevice()
+
+    async def run(self):
+        pass
+
+    def getCommand(self, axis, value):
+        range = settings['device']['range'][axis]
         value = lerp(range[0], range[1], value)
         value = clamp(int(value * 1000), 0, 999)
-        interval = int(deviceInterval * 1000)
+        interval = int(settings['device']['interval'] * 1000)
         return '{}{:03d}I{}'.format(axis, value, interval)
 
-    def updateDevice():
-        commands = [getCommand(k, v) for k, v in positions.items()]
+    def updateDevice(self):
+        commands = [self.getCommand(k, v) for k, v in self.positions.items()]
         logger.debug('devc: %s', ' '.join(commands))
-        if device:
-            device.write('{}\n'.format(' '.join(commands)).encode())
+        if self.device:
+            self.device.write('{}\n'.format(' '.join(commands)).encode())
 
-    def getActions(amount):
+class TipMenuDevice(AbstractDevice):
+    async def run(self):
+        while True:
+            amount = await self.queue.get()
+            await self.process(amount)
+            self.queue.task_done()
+
+            if self.queue.empty():
+                await self.reset(1)
+
+    async def process(self, amount):
+        actions, duration = self.getActions(amount)
+        if not actions:
+            logger.warning('devc: could not find actions for %d tip!', amount)
+            return
+
+        s = ', '.join(['[{}, {}, {}]'.format(a['axis'], a['motion'], a['frequency']) for a in actions])
+        logger.info('devc: excecuting %d tip: "%s" for %ds', amount, s, duration)
+
+        await self.execute(actions, duration)
+
+    async def execute(self, actions, duration):
+        def update(t):
+            for action in actions:
+                if t < action.get('delay', 0):
+                    continue
+
+                actionT = ((t + action.get('offset', 0)) % action['frequency']) / action['frequency']
+                self.positions[action['axis']] = self.getValue(action, actionT)
+            
+            resetT = clamp01(t / min(duration, 1))
+            for axis in idleAxes:
+                self.positions[axis] = lerp(positionsCopy[axis], self.defaultPositions[axis], resetT)
+
+            self.updateDevice()
+
+        idleAxes = [axis for axis in self.positions.keys() if not axis in [action['axis'] for action in actions]]
+        positionsCopy = self.positions.copy()
+
+        start = time.perf_counter()
+        while((time.perf_counter() - start) <= duration):
+            update(time.perf_counter() - start)
+            await asyncio.sleep(settings['device']['interval'])
+
+        update(duration)
+
+    async def reset(self, duration):
+        def update(t):
+            for k, v in self.positions.items():
+                self.positions[k] = lerp(positionsCopy[k], self.defaultPositions[k], t)
+            self.updateDevice()
+
+        logger.info('devc: resetting positions for {}s'.format(duration))
+        positionsCopy = self.positions.copy()
+
+        start = time.perf_counter()
+        while((time.perf_counter() - start) <= duration):
+            update((time.perf_counter() - start) / duration)
+            await asyncio.sleep(settings['device']['interval'])
+
+        update(1)
+
+    def getActions(self, amount):
         for option in settings['tipmenu']:
             range = option['amount']
             if (len(range) == 1 and range[0] == amount) or (len(range) == 2 and amount >= range[0] and amount <= range[1]):
                 return option['actions'], option['duration']
 
         return None, None
+    
+    def getValue(self, action, t):
+        value = self.defaultPositions[action['axis']]
 
-    def getValue(action, t):
-        t = (t % action['frequency']) / action['frequency']
         if action['motion'] == 'triangle':
             value = abs(abs(t * 2 - 1.5) - 1)
         elif action['motion'] == 'sine':
             value = -math.sin(t * math.pi * 2) / 2 + 0.5
-        elif action['motion'] == 'cosine':
-            value = -math.cos(t * math.pi * 2) / 2 + 0.5
         elif action['motion'] == 'bounce':
             x = t * math.pi * 2 - math.pi / 4
             value = -(math.sin(x)**5 + math.cos(x)**5) / 2 + 0.5
@@ -85,71 +175,117 @@ async def device(loop, queue):
 
         return clamp01(value)
 
-    async def reset(duration):
-        def update(t):
-            for k, v in positions.items():
-                positions[k] = lerp(positionsCopy[k], defaultPositions[k], t)
-            updateDevice()
+class ExcitementDevice(AbstractDevice):
+    def __init__(self, loop, queue):
+        AbstractDevice.__init__(self, loop, queue)
 
-        logger.info('devc: resetting positions for {}s'.format(duration))
-        positionsCopy = positions.copy()
-        start = time.perf_counter()
-        while((time.perf_counter() - start) <= duration):
-            update((time.perf_counter() - start) / duration)
-            await asyncio.sleep(deviceInterval)
+        self.excitiment = 0.0
+        self.lastTipTime = time.perf_counter()
+        self.dt = 0.01
+        self.tick = 0
 
-        update(1)
+    async def run(self):
+        while True:
+            if not self.queue.empty():
+                amount = await self.queue.get()
+                self.process(amount)
+                self.queue.task_done()
 
-    async def execute(actions, duration):
-        def update(t):
-            for action in actions:
-                positions[action['axis']] = getValue(action, t)
-            updateDevice()
+            await self.execute()
 
-        start = time.perf_counter()
-        while((time.perf_counter() - start) <= duration):
-            update(time.perf_counter() - start)
-            await asyncio.sleep(deviceInterval)
+    def process(self, amount):
+        self.lastTipTime = time.perf_counter()
+        self.excitiment = clamp01(self.excitiment + amount / 500)
 
-        update(duration)
+        logger.info('devc: excecuting %d tip', amount)
+    
+    async def execute(self):
+        alpha = lerp(1.5, 0.5, (time.perf_counter() - self.lastTipTime) / 12)
+        decay = math.pow(self.excitiment, math.pow(math.e, alpha)) / 8
 
-    try:
-        device = serial.Serial(devicePort, 115200)
-    except Exception as e:
-        device = None
-        logger.fatal(e)
+        self.excitiment = clamp01(self.excitiment - decay * settings['device']['interval'])
 
-    defaultPositions = {
-        'L0': 0.5,
-        'L1': 0.5,
-        'L2': 0.5,
-        'R0': 0.5,
-        'R1': 0.5,
-        'R2': 0.5,
-        'V0': 0.0,
-        'V1': 0.0
-    }
-    positions = defaultPositions.copy()
+        targetScale = lerp(0.005, 0.5, self.excitiment)
+        spread = lerp(0.25, 1, self.excitiment)
+        self.dt = lerp(self.dt, targetScale, 0.05)
+        self.tick += self.dt
+        
+        self.positions['L0'] = lerp(0.5 - spread / 2, 0.5 + spread / 2, (math.sin(self.tick) + 1) / 2) 
+        self.updateDevice()
 
-    updateDevice()
-    while True:
-        amount = await queue.get()
-        actions, duration = getActions(amount)
-        if not actions:
-            logger.warning('devc: could not find actions for %d tip!', amount)
-            continue
+        logger.info('devc: excitiment: %f decay: %f dt: %f spread: %f L0: %f', self.excitiment, decay, self.dt, spread, self.positions['L0'])
+        await asyncio.sleep(settings['device']['interval'])
 
-        s = ', '.join(['[{}, {}, {}]'.format(a['axis'], a['motion'], a['frequency']) for a in actions])
-        logger.info('devc: excecuting %d tip: "%s" for %ds', amount, s, duration)
 
-        await execute(actions, duration)
-        queue.task_done()
+class Chaturbate():
+    def __init__(self, loop, queue):
+        self.loop = loop
+        self.queue = queue
 
-        if queue.empty():
-            await reset(1)
+        response = requests.get('https://chaturbate.com/{}/'.format(settings['room']), headers={'User-Agent': 'Mozilla/5.0'})
 
-async def chaturbate(loop, queue):
-    def createMessage(method, data):
+        dossier = re.search(r'window\.initialRoomDossier\s?=\s?"(.+?)";', response.text, re.UNICODE | re.MULTILINE).group(1)
+        dossier = dossier.encode().decode("unicode-escape")
+        dossier = json.loads(dossier)
+
+        if dossier['room_status'] == 'offline':
+            logger.info('Room is offline!')
+            return
+
+        id0 = random.randint(0, 1000)
+        id1 = ''.join(random.choice(string.ascii_lowercase) for x in range(8))
+
+        self.wschat = dossier['wschat_host']
+        self.wschat = self.wschat.replace('https://', 'wss://')
+        self.wschat = '{}/{}/{}/websocket'.format(self.wschat, id0, id1)
+        self.username = dossier['chat_username']
+        self.password = dossier['chat_password']
+        self.roomPassword = dossier['room_pass']
+
+    async def run(self):
+        logger.info('connecting to websocket: %s', self.wschat)
+        async with websockets.connect(self.wschat) as ws:
+            self.connectedTime = time.perf_counter()
+            while True:
+                message = await ws.recv()
+                logger.debug('received: %s', message)
+
+                if not message:
+                    continue              
+
+                await self.process(ws, message)
+
+    async def process(self, ws, message):
+        if message[0] == 'o':
+            logger.info('sending connect')
+            await ws.send(self.createConnectedMessage())
+        elif message[0] == 'h':
+            pass
+        elif message[0] == 'a':
+            data = message[1:]
+            data = json.loads(data)
+            data = json.loads(data[0])
+            
+            if data['method'] == 'onAuthResponse':
+                result = int(data['args'][0])
+                logger.info('received onAuthResponse %d', result)
+
+                if result != 1:
+                    logger.critical('Failed to authenticate!')
+                    return
+
+                await ws.send(self.createAuthResponseMessage(result))
+            elif data['method'] == 'onNotify':
+                args = json.loads(data['args'][0])
+                if args['type'] == 'tip_alert':
+                    ignored = time.perf_counter() - self.connectedTime < 2
+                    logger.info('received tip_alert %s %d %s', args['from_username'], args['amount'], 'ignored' if ignored else '')
+                    if not ignored:
+                        loop.call_later(settings['delay'], self.pushTip, args['amount'])
+            elif data['method'] == 'onRoomMsg':
+                pass 
+
+    def createMessage(self, method, data):
         message = json.dumps({
             'method': method,
             'data': data
@@ -158,88 +294,30 @@ async def chaturbate(loop, queue):
         logger.debug('sending: %s', message)
         return message
 
-    def createConnectedMessage():
-        return createMessage('connect', {
-            'user': username,
-            'password': password,
+    def createConnectedMessage(self):
+        return self.createMessage('connect', {
+            'user': self.username,
+            'password': self.password,
             'room': settings['room'],
-            'room_password': roomPassword
+            'room_password': self.roomPassword
         })
 
-    def createAuthResponseMessage(result):
-        return createMessage('joinRoom', {
+    def createAuthResponseMessage(self, result):
+        return self.createMessage('joinRoom', {
             'room': settings['room']
         })
 
-    def pushTip(amount):
-        queue.put_nowait(amount)
-
-    response = requests.get('https://chaturbate.com/{}/'.format(settings['room']), headers={'User-Agent': 'Mozilla/5.0'})
-
-    dossier = re.search(r'window\.initialRoomDossier\s?=\s?"(.+?)";', response.text, re.UNICODE | re.MULTILINE).group(1)
-    dossier = dossier.encode().decode("unicode-escape")
-    dossier = json.loads(dossier)
-
-    if dossier['room_status'] == 'offline':
-        logger.info('Room is offline!')
-        return
-
-    wschat = dossier['wschat_host']
-    wschat = wschat.replace('https://', 'wss://')
-
-    id0 = random.randint(0, 1000)
-    id1 = ''.join(random.choice(string.ascii_lowercase) for x in range(8))
-    wschat = '{}/{}/{}/websocket'.format(wschat, id0, id1)
-
-    username = dossier['chat_username']
-    password = dossier['chat_password']
-    roomPassword = dossier['room_pass']
-
-    logger.info('connecting to websocket: %s', wschat)
-    start = time.perf_counter()
-    async with websockets.connect(wschat) as ws:
-        while True:
-            message = await ws.recv()
-            logger.debug('received: %s', message)
-
-            if not message:
-                continue              
-
-            if message[0] == 'o':
-                logger.info('sending connect')
-                await ws.send(createConnectedMessage())
-            elif message[0] == 'h':
-                pass
-            elif message[0] == 'a':
-                data = message[1:]
-                data = json.loads(data)
-                data = json.loads(data[0])
-                
-                if data['method'] == 'onAuthResponse':
-                    result = int(data['args'][0])
-                    logger.info('received onAuthResponse %d', result)
-
-                    if result != 1:
-                        logger.critical('Failed to authenticate!')
-                        return
-
-                    await ws.send(createAuthResponseMessage(result))
-                elif data['method'] == 'onNotify':
-                    args = json.loads(data['args'][0])
-                    if args['type'] == 'tip_alert':
-                        ignored = time.perf_counter() - start < 2
-                        logger.info('received tip_alert %s %d %s', args['from_username'], args['amount'], 'ignored' if ignored else '')
-                        if not ignored:
-                            loop.call_later(settings['delay'], pushTip, args['amount'])
-                elif data['method'] == 'onRoomMsg':
-                    pass
+    def pushTip(self, amount):
+        self.queue.put_nowait(amount)
 
 loop = asyncio.new_event_loop() 
 q = asyncio.Queue(loop=loop)  
 
+chaturbate = Chaturbate(loop, q)
+device = TipMenuDevice(loop, q)
 try:
-    loop.create_task(chaturbate(loop, q))
-    loop.create_task(device(loop, q))
+    loop.create_task(chaturbate.run())
+    loop.create_task(device.run())
 
     loop.run_forever()
 finally:
